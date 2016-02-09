b582303a47aed2ddf4e35fe57a9d26e28d8fe864
植原　駿 <ueponmasuta@gmail.com>
2016-02-09 21:08:24 +0900

    turn Object-c to Swift AlarmSettingViewControllerの作成　途中

--- AlarmMonster/AlarmSettingViewController.swift
+++ AlarmMonster/AlarmSettingViewController.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2016年 Shun Uehara. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AlarmSettingViewController: FoundationViewController {
+    
+    var datePicker:UIDatePicker
+    
+    // 初期表示処理
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 背景色の設定
+        self.view.backgroundColor
+            = UIColor(colorLiteralRed: 0.894, green: 0.894, blue: 0.894, alpha: 1.0)
+        // datePickerの設定
+        // 画面に配置する位置を設定
+        let pickerRect: CGRect = CGRectMake(
+            self.defaultX,
+            self.defaultY,
+            self.defaultWidth,
+            self.defaultHeight - 70)
+        datePicker = UIDatePicker(frame: pickerRect)
+        // UIDatePickerModeの設定　時間のみ
+        datePicker.datePickerMode = UIDatePickerMode.Time
+        // UIDatePickerの背景色の設定　デフォルトカラー
+        datePicker.backgroundColor
+            = UIColor(colorLiteralRed:0.302, green:0.584, blue:0.949, alpha:0.5)
+        // localeの設定 日本
+        datePicker.locale = NSLocale(localeIdentifier: "ja_JP")
+        // AlarmSettingViewControllerへDatePickerの配置
+        self.view.addSubview(datePicker)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
